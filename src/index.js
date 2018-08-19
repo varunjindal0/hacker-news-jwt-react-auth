4836c7df2f6fc8e6f1e16a96e71eae9a0653c87f
varunjindal0 <35114939+varunjindal0@users.noreply.github.com>
2018-08-19 22:07:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 // import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './components/Login'
 
-class About extends React.Component{
-    render(){
-        return (
-            <h1>About</h1>
-        )
-    }
-}
 
 ReactDOM.render(
     <Router>
